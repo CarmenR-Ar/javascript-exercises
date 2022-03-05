716e4b1e34e7ae7dce7d56b3c368bf7bb298b4bb
Carmen Ramos <carmenrramos@gmail.com>
2022-03-04 21:16:49 -0300

    Complete and test exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let word = '';
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 1; i <= num; i++) {
+        word = `${word}${string}`;
+    }
+    return word;
+}
 
-};
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
