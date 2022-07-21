8577d3ee9e64f0880f25413942f7a55e64d64f70
Carmen Ramos <carmenrramos@gmail.com>
2022-07-21 17:09:29 -0300

    Completa la funcion sumAll y habilita los tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let suma = a + b;
+    if (a<0 || b<0 || typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    }
+    else if (a < b) {
+        let x = a + 1;
+        while (x>a && x<b) {
+            suma = suma + x;
+            x++;
+        }
+        return suma;
+    }
+    else if (a > b) {
+        let x = a - 1;
+        while (x<a && x>b) {
+            suma = suma + x;
+            x--;
+        }
+        return suma;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
