f627ad6d383bd94db007c61ed62e90811d282338
Carmen Ramos <carmenrramos@gmail.com>
2022-07-21 23:15:35 -0300

    Complete and test the function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  let toCelsius = (ftemp-32)*(5/9);
+  let resultado = parseFloat(toCelsius.toFixed(1));
+  return resultado;
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+  let toFahr = ctemp*(9/5)+32;
+  return parseFloat(toFahr.toFixed(1));
 };
 
 // Do not edit below this line
